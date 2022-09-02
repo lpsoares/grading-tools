6a21b9dfa6ad07626d47c7a374d8f0e34be0876d
Luciano Pereira Soares <lpsoares@gmail.com>
2022-09-01 21:46:35 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,17 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+# From: igordsm@gmail.com
+
 setuptools.setup(
     name="grading-tools", # Replace with your own username
     version="0.0.1a",
-    author="Igor Montagner",
-    author_email="igordsm@gmail.com",
+    author="Igor Montagner / Luciano Soares",
+    author_email="lpsoaresm@gmail.com",
     description="Tools for creating autograders.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/igordsm/grading_tools",
+    url="https://github.com/lpsoares/grading_tools",
     install_requires=['psutil'],
     packages=setuptools.find_packages(),
     classifiers=[
